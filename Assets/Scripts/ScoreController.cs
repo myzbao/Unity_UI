f4e75b0b7d0b394bcdd2983d881c2d6d9060a842
Jason Walters <jason@glitchbeam.com>
2015-07-28 17:28:31 -0400

    init

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,49 @@
+﻿//-----------------------------------------
+//   Jason Walters
+//   http://glitchbeam.com
+//   @jasonrwalters
+//
+//   last edit on 7/28/2015
+//-----------------------------------------
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour
+{
+    private Text txt;
+
+    void OnEnable()
+    {
+        // reference components
+        txt = GetComponent<Text>();
+    }
+
+    void OnDisable()
+    {
+        // clear the text
+        txt.text = string.Empty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float gameScore = GameController.gameScore;
+        float gameHighScore = GameController.gameHighScore;
+
+        // update the score based on following object names
+        if (gameObject.name == "MainMenu_Text_HighScore")
+        {
+            txt.text = "Highscore " + gameHighScore;
+        }
+        else if (gameObject.name == "GamePlay_Text_Score_Time")
+        {
+            txt.text = "Score " + gameScore;
+        }
+        else if (gameObject.name == "GameOver_Text_Score_Time")
+        {
+            txt.text = "You survived " + gameScore + " seconds";
+        }
+    }
+}
